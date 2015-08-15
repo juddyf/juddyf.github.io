3f689fee77b3679a2240f64f9eb9c8d710ea00e0
juddyf <fan.juddy@gmail.com>
2015-08-15 11:41:09 -0400

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,12 +1,17 @@
 $( document ).ready(function() {
     var windowHeight = $(window).height()-50;
+    var aboutFromTop = windowHeight + 350
     var windowWidth = $(window).width();
     
     $("#cover").css("min-height", windowHeight);
+    $("about").css("top", aboutFromTop);
 
     $(window).resize(function () {
     	windowHeight = $(window).height()-50;
+    	var aboutFromTop = windowHeight + 350
     	windowWidth = $(window).width();
+    	
     	$("#cover").css("min-height", windowHeight);
+    	$var aboutFromTop = windowHeight + 350
     });
 });
