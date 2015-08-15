d8d139d2191e3077ca8cd65e2a704f0309de9d36
juddyf <fan.juddy@gmail.com>
2015-08-15 11:29:25 -0400

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,11 +1,11 @@
 $( document ).ready(function() {
-    var windowHeight = $(window).height();
+    var windowHeight = $(window).height()-50;
     var windowWidth = $(window).width();
     
     $("#cover").css("min-height", windowHeight);
 
     $(window).resize(function () {
-    	windowHeight = $(window).height();
+    	windowHeight = $(window).height()-50;
     	windowWidth = $(window).width();
     	$("#cover").css("min-height", windowHeight);
     });
