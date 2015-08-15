fd6ad780d2138c8ae1d1dabad24892e7ea6922db
juddyf <fan.juddy@gmail.com>
2015-08-15 10:46:49 -0400

    Create myjs.js

---
+++ myjs.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    var windowHeight = $(window).height();
+    var windowWidth = $(window).width();
+    
+    $(".cover").css("min-height", windowHeight);
+    });
+
+    $(window).resize(function () {
+    	windowHeight = $(window).height();
+    	windowWidth = $(window).width();
+    	$(".cover").css("min-height", windowHeight);
+    });
+});
