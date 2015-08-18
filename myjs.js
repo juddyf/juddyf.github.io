dbd688714fee6a1fe722c36da5558f96e0116ec2
juddyf <fan.juddy@gmail.com>
2015-08-18 13:31:17 -0400

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -17,8 +17,15 @@ $( document ).ready(function() {
     	windowHeight = $(window).height()+50;
     	coverHeight = $(window).height() - 50;
     	windowWidth = $(window).width();
+    	leftWidth = ($(window).width() - 905)/2;
     	
     	$("#cover").css("max-height", coverHeight);
     	$("#about").css("margin-top", windowHeight);
+    	
+    	if (windowWidth > 905) {
+            $("#logo").css("left", leftWidth);
+        } else {
+            $("#logo").css("width", windowWidth);
+        }
     });
 });
