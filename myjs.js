705cdf4d8b892f9404b5a41d8ef8959c1aec5dcc
juddyf <fan.juddy@gmail.com>
2015-08-18 13:23:27 -0400

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -2,9 +2,14 @@ $( document ).ready(function() {
     var windowHeight = $(window).height()+50;
     var coverHeight = $(window).height() - 50;
     var windowWidth = $(window).width();
+    var leftWidth = ($(window).width() - 905)/2;
     
     $("#about").css("margin-top", windowHeight);
     $("#cover").css("max-height", coverHeight);
+    
+    if (windowWidth > 905) {
+        $("#logo").css("left", leftWidth);
+    }
 
     $(window).resize(function () {
     	windowHeight = $(window).height()+50;
